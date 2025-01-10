614ab82b71f23820f6a66db97f6e9b4e00fc6d97
alexanderschaedlich <alexander.schaedlich@gmail.com>
2025-01-10 13:11:15 +0100

    DIsplay healthcheck results, use SCSS and MUI and adjust styling

--- HealthCheck/HealthCheck/src/app/app.module.ts
+++ HealthCheck/HealthCheck/src/app/app.module.ts
@@ -1,27 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FetchDataComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    HealthCheckComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatToolbarModule
   ],
-  providers: [
-    provideHttpClient()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
