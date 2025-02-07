14955f97be54b6419460fb12c9f6b911a7da7c45
alexanderschaedlich <alexander.schaedlich@gmail.com>
2025-02-07 15:43:35 +0100

    Add PWA features for HealthCheck project

--- HealthCheck/HealthCheck/src/app/app.module.ts
+++ HealthCheck/HealthCheck/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,13 @@ import { HealthCheckComponent } from './health-check/health-check.component';
     MatButtonModule,
     MatIconModule,
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
